4cfef4c79150497871f5720d6aa7521f9a5a4363
Leo <losoviz@gmail.com>
2019-09-06 14:55:01 +0800

    Implemented documentation

--- migrate/plugins/pop-posts/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
+++ migrate/plugins/pop-posts/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
@@ -12,14 +12,18 @@ class FieldValueResolver_Posts_Unit extends \PoP\ComponentModel\AbstractDBDataFi
     public function getFieldNamesToResolve(): array
     {
         return [
-			
+            'has-thumb',
+            'featuredimage',
+            'featuredimage-props',
         ];
     }
 
     public function getFieldDocumentationType(string $fieldName): ?string
     {
         $types = [
-			
+			'has-thumb' => TYPE_BOOL,
+            'featuredimage' => TYPE_ID,
+            'featuredimage-props' => TYPE_OBJECT,
         ];
         return $types[$fieldName];
     }
@@ -28,7 +32,9 @@ class FieldValueResolver_Posts_Unit extends \PoP\ComponentModel\AbstractDBDataFi
     {
         $translationAPI = TranslationAPIFacade::getInstance();
         $descriptions = [
-			
+			'has-thumb' => $translationAPI->__('', ''),
+            'featuredimage' => $translationAPI->__('', ''),
+            'featuredimage-props' => $translationAPI->__('', ''),
         ];
         return $descriptions[$fieldName];
     }
