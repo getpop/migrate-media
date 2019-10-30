5cf59598b4e421a25340b0fd3fcb0ebfd4940ca4
Leo <losoviz@gmail.com>
2019-10-30 17:22:47 +0800

    Migrated Posts

--- migrate/plugins/pop-posts/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
+++ migrate/plugins/pop-posts/library/dataload/fieldprocessors/fieldprocessor-posts-hook.php
@@ -4,12 +4,13 @@ use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\ComponentModel\Schema\SchemaDefinition;
 use PoP\ComponentModel\FieldValueResolvers\AbstractDBDataFieldValueResolver;
 use PoP\ComponentModel\FieldResolvers\FieldResolverInterface;
+use PoP\Posts\FieldResolvers\PostFieldResolver;
 
 class FieldValueResolver_Posts extends AbstractDBDataFieldValueResolver
 {
     public static function getClassesToAttachTo(): array
     {
-        return array(\PoP\Posts\FieldResolver_Posts::class);
+        return array(PostFieldResolver::class);
     }
 
     public static function getFieldNamesToResolve(): array
